cede66ae569a306b0516f1ec12a97a14bc643d62
korpez <need2know.basis846@passinbox.com>
2025-04-01 23:16:16 +0000

    Update reddit_redirect.js

--- reddit_redirect.js
+++ reddit_redirect.js
@@ -8,6 +8,8 @@
 // @run-at       document-start
 // @license      MIT
 // @namespace    https://raw.githubusercontent.com/korpez/reddit_redirect/refs/heads/main/reddit_redirect.js
+// @updateURL    https://raw.githubusercontent.com/korpez/reddit_redirect/main/reddit_redirect.js
+// @downloadURL  https://raw.githubusercontent.com/korpez/reddit_redirect/main/reddit_redirect.js
 // ==/UserScript==
 
 (function() {
